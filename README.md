042e59d4c17f688fad435986cf9c3f932add87b7
Ole Magnus Johnsen <ole.magnus@me.com>
2022-04-17 00:41:00 +0200

    you can now stop and reserve a machine. see your balance and purchase history

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # CLI for Appwash by Miele
 
-Still very much a work in progress
+A way to interact with the AppWash API through your terminal.
+
+## Features
+- Reserve a machine
+- Stop a reserved machine
+- See purchase history
+- Check your balance
+- List machines at your location and their status
 
 Replacement for [miele-cli](https://github.com/omfj/miele-cli).
+
+# Made by
+<table>
+    <td align="center">
+        <a href="https://github.com/omfj">
+            <img height="100" src="https://avatars.githubusercontent.com/u/32321558?v=4" />
+            <br>
+            <sub><b>Ole Magnus</b></sub>
+        </a>
+    </td>
+</table>
--- appwash-cli.py
+++ appwash-cli.py
@@ -6,24 +6,14 @@ import os
 from datetime import datetime
 from rich.console import Console
 from rich.prompt import Prompt
-
-# Functions
-# =========
-# login
-# print_help
-# print_machines
-# print_whoami
-# restart
-# clear
-# logout
-
+from rich.table import Table
 
 # Initialize console
 console = Console()
 prompt = Prompt()
 
 logged_in = False
-user_agent = "appwash cli github.com/omfj/appwash-cli"
+user_agent = "appwash-cli github.com/omfj/appwash-cli"
 
 def print_whoami(secrets=False):
     try:
@@ -90,12 +80,7 @@ def logout():
     console.print("You are now logged out.")
 
 
-def print_machines():
-
-    if not logged_in:
-        console.print("You are not logged in.")
-        return
-
+def get_machines():
     # Login headers
     headers = { "User-Agent": f"{user_agent}", "Referer": "https://appwash.com/", "token": f"{account_info['token']}", "language": "NO", "platform": "appWash", "DNT": "1" }
     json_data = { "serviceType": "WASHING_MACHINE" }
@@ -110,43 +95,165 @@ def print_machines():
         machine_data = data_response_json["data"]
     except Exception:
         print(f"Error {data_response.status_code}: Could not get machine list", end="\n\n")
-        exit()
+        return
 
-    # Print all machines in the list
+    return machine_data
+
+def print_machines():
+    if not logged_in:
+        console.print("You are not logged in.")
+        return
+
+    machine_data = get_machines()
+
     for machine in machine_data:
         machine_id = machine["externalId"]
         machine_state = machine["state"]
-        if machine_state == "OCCUPIED":
+        if machine_state in ["OCCUPIED", "STOPPABLE"]:
             last_session_start = machine["lastSessionStart"]
             last_session_start = datetime.fromtimestamp(last_session_start).strftime("%Y-%m-%d %H:%M:%S")
-            console.print(f"{machine_id} - {machine_state} - STARTED: {last_session_start}")
+
+            if machine_state == "STOPPABLE":
+                console.print(f"-> {machine_id} - [bold green]{machine_state}[/bold green] - STARTED: {last_session_start}")
+            else:
+                console.print(f"{machine_id} - {machine_state} - STARTED: {last_session_start}")
         else:
             console.print(f"{machine_id} - {machine_state}")
 
-def reserve():
+
+
+def reserve_machine():
     if not logged_in:
         console.print("You are not logged in.")
         return
 
     console.print("What machine do you want to reserve?")
-    machine_id = prompt.ask("Machine ID: ")
-    reserve_headers = {}
+    machine_id = prompt.ask("Machine ID")
 
+    are_you_sure = prompt.ask("Are you sure you want to reserve machine {machine_id}? (y/N)")
+
+    if not are_you_sure == "y":
+        return
+
+    reserve_machine_headers = { 'User-Agent': f"{user_agent}", 'Referer': 'https://appwash.com/', 'language': 'NO', 'token': f"{account_info['token']}", 'platform': 'appWash', 'Origin': 'https://appwash.com', 'DNT': '1' }
+    reserve_machine_json_data = { 'objectId': None, 'objectLength': None, 'objectName': None, 'nrOfPersons': None, 'freeFormQuestionValue': None, 'comment': None, 'sourceChannel': 'WEBSITE' }
+    reserve_machine_response = requests.post(f'https://www.involtum-services.com/api-rest/connector/{machine_id}/start', headers=reserve_machine_headers, json=reserve_machine_json_data)
+
+    error_code = reserve_machine_response.json()["errorCode"]
+
+    if error_code == 0:
+        console.print(f"Successfully reserved machine {machine_id}")
+    else:
+        console.print(f"Error {error_code}: Could not reserve machine {machine_id}")
+
+
+def stop_machine():
+    if not logged_in:
+        console.print("You are not logged in.")
+        return
+
+    console.print("What machine do you want to stop?")
+    machine_id = prompt.ask("Machine ID")
+
+    are_you_sure = prompt.ask("Are you sure you want to stop machine {machine_id}? (y/N)")
+
+    if not are_you_sure == "y":
+        return
+
+    reserve_machine_headers = { 'User-Agent': f"{user_agent}", 'Referer': 'https://appwash.com/', 'language': 'NO', 'token': f"{account_info['token']}", 'platform': 'appWash', 'Origin': 'https://appwash.com', 'DNT': '1' }
+    reserve_machine_json_data = { 'objectId': None, 'objectLength': None, 'objectName': None, 'nrOfPersons': None, 'freeFormQuestionValue': None, 'comment': None, 'sourceChannel': 'WEBSITE' }
+    reserve_machine_response = requests.post(f'https://www.involtum-services.com/api-rest/connector/{machine_id}/stop', headers=reserve_machine_headers, json=reserve_machine_json_data)
+
+    error_code = reserve_machine_response.json()["errorCode"]
+    print(reserve_machine_response.json())
+
+    if error_code == 0:
+        console.print(f"Successfully stopped machine {machine_id}")
+    else:
+        console.print(f"Error {error_code}: Could not stop machine {machine_id}")
+
+
+def print_balance():
+    if not logged_in:
+        console.print("You are not logged in.")
+        return
+
+
+    balance_headers = { 'User-Agent': f"{user_agent}", 'Referer': 'https://appwash.com/', 'token': f"{account_info['token']}", 'language': 'NO', 'platform': 'appWash', 'Origin': 'https://appwash.com', 'DNT': '1' }
+
+    response = requests.get('https://www.involtum-services.com/api-rest/account/getprepaid', headers=balance_headers)
+    response_json = response.json()
+
+    error_code = response_json["errorCode"]
+
+    if error_code == 0:
+        balance = response_json["balanceCents"] / 100
+        currency = response_json["currency"]
+
+        console.print(f"Your balance is {balance} {currency}.")
+    else:
+        console.print(f"Error {error_code}: Could not get balance.")
+
+
+def get_history():
+    history_headers = { 'User-Agent': f"{user_agent}", 'Referer': 'https://appwash.com/', 'token': f"{account_info['token']}", 'language': 'NO', 'platform': 'appWash', 'Origin': 'https://appwash.com', 'DNT': '1' }
+
+    history_response = requests.get('https://www.involtum-services.com/api-rest/account/getprepaidmutations', headers=history_headers)
+
+    try:
+        print("Successfully fetched purchase history.", end="\n\n")
+        purchase_history = history_response.json()
+    except Exception:
+        print(f"Error {data_response.status_code}: Could not get machine list", end="\n\n")
+        return
+
+    return purchase_history
+
+
+def print_history():
+    if not logged_in:
+        console.print("You are not logged in.")
+        return
+
+    purchase_history = get_history()
+
+    purchase_history_table = Table(title="Purchase history")
+    purchase_history_table.add_column("Description", justify="center")
+    purchase_history_table.add_column("Date",        justify="center")
+    purchase_history_table.add_column("Amount",      justify="center")
+    purchase_history_table.add_column("Bal. Before", justify="center")
+    purchase_history_table.add_column("Bal. After",  justify="center")
+
+    for purchase_info in purchase_history["data"]:
+        time = str(datetime.fromtimestamp(purchase_info["mutationTimestamp"]).strftime("%Y-%m-%d %H:%M:%S"))
+        currency = purchase_info["currency"]
+        amount = str(purchase_info["mutationCents"] / 100)
+        balance_before = str(purchase_info["balanceCentsBefore"] / 100)
+        balance_after = str(purchase_info["balanceCentsAfter"] / 100)
+        description = purchase_info["mutationDescription"].capitalize()
+
+        purchase_history_table.add_row(description, time, f"{amount} {currency}", balance_before, balance_after)
+
+    console.print(purchase_history_table)
 
 def print_help():
     console.print("Available commands:")
-    console.print("'list' - List all machines")
-    console.print("'stop' - Stop the program")
+    console.print("'list'    - List all machines")
+    console.print("'stop'    - Stop the program")
     console.print("'restart' - Restarts the program")
-    console.print("'help' - Print this help")
-    console.print("'whoami' - Print your account information")
-    console.print("'login' - Login to the appwash service")
+    console.print("'help'    - Print this help")
+    console.print("'whoami'  - Print your account information")
+    console.print("'login'   - Login to the appwash service")
     console.print("'reserve' - Reserve a machine")
-    console.print("'clear' - Clears the screen")
+    console.print("'stop' .  - Stops a machine")
+    console.print("'balance' - Print your balance")
+    console.print("'history' - Print your purchase history")
+    console.print("'clear'   - Clears the screen")
 
 
 def restart():
     os.execv(sys.argv[0], sys.argv)
+
 
 def clear():
     os.system("clear")
@@ -162,7 +269,7 @@ def exec_command(command):
     match (command):
         case "list":
             print_machines()
-        case "stop" | "exit" | "quit" | "q" | "e":
+        case "exit" | "quit" | "q" | "e":
             print("Bye bye!")
             exit()
         case "restart" | "r":
@@ -172,7 +279,9 @@ def exec_command(command):
         case "help":
             print_help()
         case "reserve" | "re":
-            reserve()
+            reserve_machine()
+        case "stop" | "s":
+            stop_machine()
         case "whoami":
             if "--secrets" in args:
                 print_whoami(True)
@@ -185,6 +294,10 @@ def exec_command(command):
                 login()
         case "logout":
             logout()
+        case "balance" | "bal":
+            print_balance()
+        case "history" | "h":
+            print_history()
         case _:
             console.print("Unknown command")
 
